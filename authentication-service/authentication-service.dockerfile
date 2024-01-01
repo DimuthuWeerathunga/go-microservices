faaeef9e14556e4042d6ce50c2fa51dda8be2562
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2024-01-01 22:19:49 +0530

    logging

--- authentication-service/authentication-service.dockerfile
+++ authentication-service/authentication-service.dockerfile
@@ -1,22 +1,7 @@
-# base go image
-# FROM golang:1.18-alpine as builder
-
-# RUN mkdir /app
-
-# COPY . /app
-
-# WORKDIR /app
-
-# RUN CGO_ENABLED=0 go build -o brokerApp ./cmd/api
-
-# RUN chmod +x /app/brokerApp
-# commented since i'm building the executable locally
-
-# build a tiny docker image
 FROM alpine:latest
 
 RUN mkdir /app
 
 COPY authApp /app
 
-CMD [ "/app/authApp" ]
+CMD [ "/app/authApp"]
--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -19,27 +19,27 @@ const webPort = "80"
 var counts int64
 
 type Config struct {
-	DB     *sql.DB
+	DB *sql.DB
 	Models data.Models
 }
 
 func main() {
 	log.Println("Starting authentication service")
 
-	// connect to db
+	// connect to DB
 	conn := connectToDB()
 	if conn == nil {
-		log.Panic("Can't connect to postgress")
+		log.Panic("Can't connect to Postgres!")
 	}
 
 	// set up config
 	app := Config{
-		DB:     conn,
+		DB: conn,
 		Models: data.New(conn),
 	}
 
 	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%s", webPort),
+		Addr: fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
@@ -59,20 +59,20 @@ func openDB(dsn string) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return db, nil
 }
 
 func connectToDB() *sql.DB {
 	dsn := os.Getenv("DSN")
-	fmt.Println(dsn)
 
 	for {
 		connection, err := openDB(dsn)
 		if err != nil {
-			log.Println("Postgres not ready ...")
+			log.Println("Postgres not yet ready ...")
 			counts++
 		} else {
-			log.Println("Connected to postgres")
+			log.Println("Connected to Postgres!")
 			return connection
 		}
 
@@ -81,7 +81,7 @@ func connectToDB() *sql.DB {
 			return nil
 		}
 
-		log.Println("Backing off for two seconds...")
+		log.Println("Backing off for two seconds....")
 		time.Sleep(2 * time.Second)
 		continue
 	}
